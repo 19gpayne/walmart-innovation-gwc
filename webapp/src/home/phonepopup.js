218fff090d9421adb94c8273ee815e19b5d227c8
Gwendolyn Payne <gwenpayne2012@gmail.com>
2021-07-25 18:00:59 -0400

    open hbr in new tab

--- webapp/src/home/phonepopup.js
+++ webapp/src/home/phonepopup.js
@@ -44,7 +44,7 @@ export const CartPopup = ({close, next}) => (
             <Close onClick={close}><CloseOutlined/></Close>
             <Title>The shopping cart is also equipment from 1937</Title>
             <Body>
-                It is probably time for an upgrade. Today's consumer value reduced effort, reduced time, quality, and organization <a href="https://hbr.org/2016/08/the-30-things-customers-really-value">according to Harvard Business Review</a>.
+                It is probably time for an upgrade. Today's consumer value reduced effort, reduced time, quality, and organization <a href="https://hbr.org/2016/08/the-30-things-customers-really-value" target='_blank'>according to Harvard Business Review</a>.
                 Explore how the Walcart Connect can meet consumers' values, wants, and needs in 2021.
             </Body>
             <Icon><img src='https://cdn.pixabay.com/photo/2017/08/09/10/00/shopping-cart-2614015_960_720.png' height="auto" width="30%" style={{textAlign: "center", display: 'flex'}} alt='Phone'/></Icon>
